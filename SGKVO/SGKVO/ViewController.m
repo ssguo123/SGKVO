086f651ebe9c9b8d203371dbb9ef46a9c62dd7c2
ssguo123 <13051698903@163.com>
2018-08-21 13:59:32 +0800

    第一个工程

---
+++ SGKVO/SGKVO/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  SGKVO
+//
+//  Created by ssg on 2018/8/21.
+//  Copyright © 2018年 visionvera. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
---
+++ SGKVO/SGKVO/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  SGKVO
+//
+//  Created by ssg on 2018/8/21.
+//  Copyright © 2018年 visionvera. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
