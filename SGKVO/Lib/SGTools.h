99615f83008e6c97981cf69f4cd4bb3804c187bf
ssguo123 <13051698903@163.com>
2020-05-07 17:48:05 +0800

    fef3r

---
+++ SGKVO/Lib/SGTools.h
@@ -0,0 +1,19 @@
+//
+//  SGTools.h
+//  SGKVO
+//
+//  Created by ssg on 2020/5/7.
+//  Copyright © 2020 visionvera. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface SGTools : NSObject
+
+- (void)test;
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ SGKVO/Lib/SGTools.m
@@ -0,0 +1,17 @@
+//
+//  SGTools.m
+//  SGKVO
+//
+//  Created by ssg on 2020/5/7.
+//  Copyright © 2020 visionvera. All rights reserved.
+//
+
+#import "SGTools.h"
+
+@implementation SGTools
+
+- (void)test{
+    NSLog(@"12123312344");
+}
+
+@end
